beca3c68216c53a2d6b83546a969d6026c79a436
Tomoyuki Ohta <toohta@microsoft.com>
2023-10-12 14:36:19 +0900

    exclude fmt check

--- main.tf
+++ main.tf
@@ -31,5 +31,5 @@ resource "azurerm_virtual_network" "name" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg-terraform.name  
+  resource_group_name = var.resource_group_name
 }
